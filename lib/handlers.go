3044a991df52d71567591e85fdabd7ad7e58c5e8
Sebastian Gryczan <sgryczan@netapp.com>
2020-05-19 23:17:28 -0600

    disable verbose mode

--- lib/handlers.go
+++ lib/handlers.go
@@ -26,7 +26,7 @@ func HandleMutate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// mutate the request
-	mutated, err := Mutate(body, true)
+	mutated, err := Mutate(body, false)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
